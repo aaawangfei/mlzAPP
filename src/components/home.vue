50813b2e4116dab7c12cf26c3aa4f53a43ac3255
神煤科技01 <shenmeikeji01@shenmeikeji01deMacBook-Pro.local>
2019-10-12 17:44:47 +0800

    first

---
+++ src/components/home.vue
@@ -0,0 +1,31 @@
+<template>
+  <van-tabbar v-model="active">
+  <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+  <van-tabbar-item icon="apps-o">分类</van-tabbar-item>
+  <van-tabbar-item icon="orders-o">求购</van-tabbar-item>
+  <van-tabbar-item icon="cart-o">购物车</van-tabbar-item>
+	<van-tabbar-item icon="contact">我的</van-tabbar-item>
+</van-tabbar>
+</template>
+
+<script>
+import Vue from 'vue';
+import Vant from 'vant';
+import { Tabbar, TabbarItem } from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+Vue.use(Tabbar).use(TabbarItem);
+export default {
+  data() {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+	
+</style>
