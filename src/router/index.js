b4eadc0061af49b5c42a515fbeebee9c35ddd391
sor <546158028@qq.com>
2019-10-17 11:31:43 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import myRouter from './modules/my'
 import loginRouter from './modules/login'
+import orders from './modules/buying'
 Vue.use(Router)
 export const homeRouter=[
     {
@@ -30,7 +31,7 @@ export const homeRouter=[
     	  component: () => import('@/components/orders/orders'),
     	}
   ];
-let routes = new Set([...homeRouter,...myRouter,...loginRouter]);
+let routes = new Set([...homeRouter,...myRouter,...loginRouter,...orders]);
 export default new Router({
   routes: routes
 })
